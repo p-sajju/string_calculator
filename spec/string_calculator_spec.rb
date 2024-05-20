76b08703b1fc1483214881ee553992e40d0b3784
Sajjan Patel <Sajjanpatel123@gmail.com>
2024-05-20 21:47:28 +0530

    Add test case for 0 in empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,2 +1,12 @@
 # string_calculator_spec.rb
 require 'string_calculator'
+
+RSpec.describe StringCalculator do
+  let(:calculator) { StringCalculator.new }
+
+  describe '#add' do
+    it 'returns 0 for an empty string' do
+      expect(calculator.add("")).to eq(0)
+    end
+  end
+end
