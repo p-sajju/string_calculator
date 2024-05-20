2a7ce011bb1c4f278deffa8b334978769c7e8f62
Sajjan Patel <Sajjanpatel123@gmail.com>
2024-05-20 21:50:01 +0530

    Write test case for the sum of two numbers separated by comma

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -12,5 +12,9 @@ RSpec.describe StringCalculator do
     it 'returns the number for a single number' do
       expect(calculator.add("1")).to eq(1)
     end
+
+    it 'returns the sum of two numbers separated by comma' do
+      expect(calculator.add("1,2")).to eq(3)
+    end
   end
 end
