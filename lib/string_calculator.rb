499125424020e9670bff2f1b8d236f4cd5935292
Sajjan Patel <sajjanpatel123@gmail.com>
2025-05-27 22:28:12 +0530

    Implement support for custom single-character delimiters

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,7 +2,13 @@
 class StringCalculator
 	def self.add(numbers)
 		return 0 if numbers.empty?
-        nums = numbers.split(/[\n,]/).map(&:to_i)
+        if numbers.start_with?("//")
+            delimiter, numbers = numbers.match(%r{//(.)\n(.*)})[1, 2]
+            nums = numbers.split(delimiter).map(&:to_i)
+        else
+            nums = numbers.split(/[\n,]/).map(&:to_i)
+        end
+
         nums.sum
 	end
 end
