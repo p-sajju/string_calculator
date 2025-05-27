b8ea925e4655de1ac5717b2991d0cc60d0581f9d
Sajjan Patel <sajjanpatel123@gmail.com>
2025-05-27 22:17:20 +0530

    RSpec test for empty string and single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it "returns the number itself for a single number" do
+        calculator = StringCalculator.new
+        expect(calculator.add("5")).to eq(5)
+    end
 end
