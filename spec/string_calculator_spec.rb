729f06e29da377cfd6a00638046dc9cd5820d4a4
Sajjan Patel <sajjanpatel123@gmail.com>
2025-05-27 22:24:33 +0530

    Add test for newline and comma mixed delimiters

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -14,4 +14,12 @@ RSpec.describe StringCalculator do
     it "returns the sum of two comma-separated numbers" do
         expect(StringCalculator.add("1,2")).to eq(3)
     end
+
+    it "returns the sum of an unknown amount of comma-separated numbers" do
+        expect(StringCalculator.add("1,2,3,4")).to eq(10)
+    end
+    
+    it "supports newlines as valid delimiters along with commas" do
+        expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
 end
