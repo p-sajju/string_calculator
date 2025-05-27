614bd41dfd9db49a1da0f58b5f6ff2d586b2e379
Sajjan Patel <sajjanpatel123@gmail.com>
2025-05-27 22:36:53 +0530

    Add code for custom delimiter of any length

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -3,8 +3,14 @@ class StringCalculator
 	def self.add(numbers)
 		return 0 if numbers.empty?
         if numbers.start_with?("//")
-            delimiter, numbers = numbers.match(%r{//(.)\n(.*)})[1, 2]
-            nums = numbers.split(delimiter).map(&:to_i)
+            if numbers =~ %r{//\[(.+?)\]\n(.*)}
+                delimiter = Regexp.escape($1)
+                numbers = $2
+                nums = numbers.split(/#{delimiter}/).map(&:to_i)
+            else
+                delimiter, numbers = numbers.match(%r{//(.)\n(.*)})[1, 2]
+                nums = numbers.split(delimiter).map(&:to_i)
+            end
         else
             nums = numbers.split(/[\n,]/).map(&:to_i)
         end
